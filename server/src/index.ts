f83c8c07c3f53e909da47171cc8a0a20ac559afb
Bawanjot Singh <jotkalra5@gmail.com>
2025-08-20 15:48:08 +0530

    Update index.ts cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,6 +31,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:3001",
       "http://127.0.0.1:3000",
+      "https://pos-f.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
